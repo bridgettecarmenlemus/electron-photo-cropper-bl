396351cd95866e228cf2510e5e340e9deea26673
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-08-25 10:34:34 -0400

    working cropper and saving

--- src/renderer/components/Photo.tsx
+++ src/renderer/components/Photo.tsx
@@ -1,39 +1,66 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import Cropper from 'react-easy-crop';
-import { readFile } from '../../helpers/images';
+import { Area } from 'react-easy-crop/types'
+import { cropImageData, readFile } from '../../helpers/images';
 
 export default function Photo() {
   const [imageSrc, setImageSrc] = useState(null); // file data
   const [crop, setCrop] = useState({ x: 0, y: 0 });
-  const [zoom, setZoom]= useState(1);
-  // const [filename, setFileName] = useState(null); // file address
+  const [zoom, setZoom] = useState(1);
+  const [filename, setFileName] = useState(null); // file address
+
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
+  const onCropComplete = useCallback ((_croppedArea:Area , currentCroppedAreaPixels: Area) => {
+    setCroppedAreaPixels(currentCroppedAreaPixels)
+  }, [])
+  // const [filename, setFilename] = useState(null); // file address
   const handleFileChange = async (e: any) => {
     if (e.target.files && e.target.files.length) {
-      // we got a file . . .
-      const file = e.target.file[0];
-      // setFileName(file.path);
-      //get the image data from the file and then i want to set the image source to that image data
+      // we got a file...
+      const file = e.target.files[0];
+      setFileName(file.path);
+      // get the image data from the file
       const imageData: any = await readFile(file);
+      // setImageSrc to that image data
       setImageSrc(imageData);
     }
+  };
+  const handleSave = async () => {
+    //first save the crop image then reset the interface
+    // first create the cropped image data using a canvas...
+    const base64data= await cropImageData(imageSrc, croppedAreaPixels!)
+      .catch (console.error);
+    // create a new file name
+    const newFileName= filename + '-cropped.png';
+    // then send those results to saveImage via ipcRender event
+    window.electron.saveCroppedImage([newFileName, base64data])
+    // then reset the interface
+    setImageSrc(null);
+    setZoom(1);
+    setCrop({ x: 0, y: 0 });
   };
   if (!imageSrc) {
     return (
       <>
-        <h1>Please Choose Photo to Crop</h1>
+        <h1>Please choose photo to crop</h1>
         <input type="file" accept="image/*" onChange={handleFileChange} />
       </>
     );
   }
   return (
     <>
-    <Cropper
-      image={imageSrc}
-      crop={crop}
-      zoom={zoom}
-      onCropChange={setCrop}
-      onZoomChange={setZoom}
+      <Cropper
+        image={imageSrc}
+        crop={crop}
+        zoom={zoom}
+        aspect={1}
+        onCropChange={setCrop}
+        onZoomChange={setZoom}
+        onCropComplete={onCropComplete}
       />
+      <button className="save-btn" onClick={handleSave}>
+        Save
+      </button>
     </>
   );
 }
